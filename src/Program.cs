ac67f0920e9c24028591b7696b2d64f0e9d1b7f7
Ryan Campbell <89273172+bigtallcampbell@users.noreply.github.com>
2024-07-22 10:51:32 -0500

    fixing sequence typing (#8)

--- src/Program.cs
+++ src/Program.cs
@@ -44,7 +44,6 @@ public class Program {
                 await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
             });
         });
-        app.Run();
 
         // Add a middleware to catch exceptions and stop the host gracefully
         app.Use(async (context, next) => {
@@ -58,5 +57,7 @@ public class Program {
                 lifetime?.StopApplication();
             }
         });
+
+        app.Run();
     }
 }
