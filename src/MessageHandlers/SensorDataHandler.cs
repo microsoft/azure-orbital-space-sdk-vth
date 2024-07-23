9e54a3d249d6571b39853f37a2a73c346ba67879
alianides <alianides@microsoft.com>
2024-07-23 21:53:22 +0000

    updated to include status within logging responses

--- src/MessageHandlers/SensorDataHandler.cs
+++ src/MessageHandlers/SensorDataHandler.cs
@@ -6,27 +6,27 @@ public partial class MessageHandler<T> {
     private void SensorDataHandler(MessageFormats.HostServices.Sensor.SensorData? message, MessageFormats.Common.DirectToApp fullMessage) {
         if (message == null) return;
         using (var scope = _serviceProvider.CreateScope()) {
-            _logger.LogInformation("Processing message type '{messageType}' from '{sourceApp}' (trackingId: '{trackingId}' / correlationId: '{correlationId}')", message.GetType().Name, fullMessage.SourceAppId, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId);
+            _logger.LogInformation("Processing message type '{messageType}' from '{sourceApp}' (trackingId: '{trackingId}' / correlationId: '{correlationId}' / status: '{status}')", message.GetType().Name, fullMessage.SourceAppId, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId, message.ResponseHeader.Status);
 
-            _logger.LogDebug("Passing message '{messageType}' to plugins (trackingId: '{trackingId}' / correlationId: '{correlationId}')", message.GetType().Name, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId);
+            _logger.LogDebug("Passing message '{messageType}' to plugins (trackingId: '{trackingId}' / correlationId: '{correlationId}' / status: '{status}')", message.GetType().Name, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId, message.ResponseHeader.Status);
 
             MessageFormats.HostServices.Sensor.SensorData? pluginResult =
                          _pluginLoader.CallPlugins<MessageFormats.HostServices.Sensor.SensorData?, Plugins.PluginBase>(
                              orig_request: message,
                              pluginDelegate: _pluginDelegates.SensorData);
 
-            _logger.LogDebug("Plugins finished processing '{messageType}' (trackingId: '{trackingId}' / correlationId: '{correlationId}')", message.GetType().Name, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId);
+            _logger.LogDebug("Plugins finished processing '{messageType}' (trackingId: '{trackingId}' / correlationId: '{correlationId}' / status: '{status}')", message.GetType().Name, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId, message.ResponseHeader.Status);
 
             if (pluginResult == null) {
-                _logger.LogInformation("Plugins nullified '{messageType}'.  Dropping Message (trackingId: '{trackingId}' / correlationId: '{correlationId}')", message.GetType().Name, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId);
+                _logger.LogInformation("Plugins nullified '{messageType}'.  Dropping Message (trackingId: '{trackingId}' / correlationId: '{correlationId}' / status: '{status}')", message.GetType().Name, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId, message.ResponseHeader.Status);
                 return;
             }
 
             if (_appConfig.ENABLE_ROUTING_TO_MTS) {
-                _logger.LogInformation("Routing message type '{messageType}' to '{appId}' (trackingId: '{trackingId}' / correlationId: '{correlationId}')", message.GetType().Name, $"platform-{nameof(MessageFormats.Common.PlatformServices.Mts)}", message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId);
+                _logger.LogInformation("Routing message type '{messageType}' to '{appId}' (trackingId: '{trackingId}' / correlationId: '{correlationId}' / status: '{status}')", message.GetType().Name, $"platform-{nameof(MessageFormats.Common.PlatformServices.Mts)}", message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId, message.ResponseHeader.Status);
                 _sensorDataTransmitService.QueueSensorData(pluginResult);
             } else {
-                _logger.LogInformation("Routing message type '{messageType}' to '{appId}' (trackingId: '{trackingId}' / correlationId: '{correlationId}')", message.GetType().Name, fullMessage.SourceAppId, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId);
+                _logger.LogInformation("Routing message type '{messageType}' to '{appId}' (trackingId: '{trackingId}' / correlationId: '{correlationId}' / status: '{status}')", message.GetType().Name, fullMessage.SourceAppId, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId, message.ResponseHeader.Status);
                 _client.DirectToApp(appId: fullMessage.SourceAppId, message: pluginResult);
             }
         };
--- src/MessageHandlers/SensorsAvailableResponseHandler.cs
+++ src/MessageHandlers/SensorsAvailableResponseHandler.cs
@@ -6,7 +6,7 @@ public partial class MessageHandler<T> {
     private void SensorsAvailableResponseHandler(MessageFormats.HostServices.Sensor.SensorsAvailableResponse? message, MessageFormats.Common.DirectToApp fullMessage) {
         if (message == null) return;
         using (var scope = _serviceProvider.CreateScope()) {
-            _logger.LogInformation("Processing message type '{messageType}' from '{sourceApp}' (trackingId: '{trackingId}' / correlationId: '{correlationId}')", message.GetType().Name, fullMessage.SourceAppId, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId);
+            _logger.LogInformation("Processing message type '{messageType}' from '{sourceApp}' (trackingId: '{trackingId}' / correlationId: '{correlationId}' / status: '{status}')", message.GetType().Name, fullMessage.SourceAppId, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId, message.ResponseHeader.Status);
 
             MessageFormats.HostServices.Sensor.TaskingPreCheckResponse returnResponse = new() {
                 ResponseHeader = new() {
@@ -16,7 +16,7 @@ public partial class MessageHandler<T> {
                 }
             };
 
-            _logger.LogDebug("Passing message '{messageType}' and '{responseType}' to plugins (trackingId: '{trackingId}' / correlationId: '{correlationId}')", message.GetType().Name, returnResponse.GetType().Name, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId);
+            _logger.LogDebug("Passing message '{messageType}' and '{responseType}' to plugins (trackingId: '{trackingId}' / correlationId: '{correlationId}' / status: '{status}')", message.GetType().Name, returnResponse.GetType().Name, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId, message.ResponseHeader.Status);
 
 
             MessageFormats.HostServices.Sensor.SensorsAvailableResponse? pluginResult =
@@ -24,7 +24,7 @@ public partial class MessageHandler<T> {
                              orig_request: message,
                              pluginDelegate: _pluginDelegates.SensorsAvailableResponse);
 
-            _logger.LogDebug("Plugins finished processing '{messageType}' and '{responseType}' (trackingId: '{trackingId}' / correlationId: '{correlationId}')", message.GetType().Name, returnResponse.GetType().Name, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId);
+            _logger.LogDebug("Plugins finished processing '{messageType}' and '{responseType}' (trackingId: '{trackingId}' / correlationId: '{correlationId}' / status: '{status}')", message.GetType().Name, returnResponse.GetType().Name, message.ResponseHeader.TrackingId, message.ResponseHeader.CorrelationId, message.ResponseHeader.Status);
         };
     }
 }
