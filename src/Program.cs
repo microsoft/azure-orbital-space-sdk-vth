d333deb7028ed11e54cef0544df0d2d148d28b78
Ryan Campbell <89273172+bigtallcampbell@users.noreply.github.com>
2024-07-31 16:49:51 -0500

    adding grpc health check (#10)

--- src/Program.cs
+++ src/Program.cs
@@ -40,6 +40,7 @@ public class Program {
         app.UseRouting();
         app.UseEndpoints(endpoints => {
             endpoints.MapGrpcService<Microsoft.Azure.SpaceFx.Core.Services.MessageReceiver>();
+            endpoints.MapGrpcHealthChecksService();
             endpoints.MapGet("/", async context => {
                 await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
             });
